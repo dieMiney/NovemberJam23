af43adcd53b592a69bb2d80c21f0b9244f3a6c3a
David Fischer <davidvfischer@gmail.com>
2023-11-12 19:16:11 +0100

    added input text field and red and blue cube

---
+++ Assets/Scripts/HUDInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HUDInput : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Enter()
+    {
+        // toggle inputfield activated
+        
+    }
+}
